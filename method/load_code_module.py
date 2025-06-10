b2be34ad7d041ecca115526fe6e75b35061a7de8
Geng Zhigao <zhigaogeng.johnson@gmail.com>
2025-06-10 11:36:36 +0800

    feat: update code load module

--- method/load_code_module.py
+++ method/load_code_module.py
@@ -1,15 +1,16 @@
 import subprocess
+import os
 
 
 def load_code_module(project_dir: str) -> str:
     """
     输入: 项目根目录
-    输出: 解析后的JSON数据（包含类签名、成员变量、方法信息、依赖图等）
+    输出: 解析后的JSON数据位置（包含类签名、成员变量、方法信息、依赖图等）
     """
 
-    jar_path = '../model/CodeSumCCA-javaparser.jar'
+    jar_path = os.getcwd() + '/model/CodeSumCCA-javaparser-1.0-SNAPSHOT.jar'
     command = ["java", "-jar", jar_path, project_dir]
 
     subprocess.run(command, capture_output=True, text=True)
 
-    return project_dir.split('/')[-1]
+    return f"tmp/output/{project_dir.split('/')[-1]}"
